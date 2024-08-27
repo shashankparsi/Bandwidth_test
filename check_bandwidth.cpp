2a63f657efb66f334b6902a34982aca1e60992f8
shashank parsi <parsishashank351@gmail.com>
2024-08-27 12:00:40 +0530

    Add files via upload

---
+++ check_bandwidth.cpp
@@ -0,0 +1,98 @@
+#include<stdio.h>
+#include<hip/hip_runtime.h>
+#define ITERATIONS 10000
+void checkbandwidth(hipStream_t& stream,size_t datasize)
+{
+	int devicecnt;
+	hipGetDeviceCount(&devicecnt);
+	printf("Number of GPU's in system are : %d\n",devicecnt);
+	float *hdata = new float[datasize];
+	if(hdata == NULL)
+	{
+		printf("memory allocation failed on HOST\n");
+		return;
+	}
+	for(size_t i=0;i<datasize;i++)
+	{
+		hdata[i]=static_cast<float>(i);
+	}
+	float *ddata=NULL;
+	hipMalloc((void**)&ddata,datasize*sizeof(float));
+	hipStreamSynchronize(stream);
+	hipEvent_t start,stop;
+	hipEventCreate(&start);
+	hipEventCreate(&stop);
+	hipEventRecord(start,stream);
+	for(int i=0;i<ITERATIONS;i++)
+	{
+		hipMemcpyAsync(ddata,hdata,datasize*sizeof(float),hipMemcpyHostToDevice,stream);
+	}
+	hipEventRecord(stop,stream);
+	hipStreamSynchronize(stream);
+	float milli_sec = 0;
+	hipEventElapsedTime(&milli_sec,start,stop);
+	const float bandwidth = ((float(datasize)*sizeof(float)*float(ITERATIONS))/(milli_sec * 1e6));
+	printf("CPU to GPU Bandwidth is :%0.3f GB/s\n",bandwidth);
+	
+	hipEventCreate(&start);
+        hipEventCreate(&stop);
+        hipEventRecord(start,stream);
+        for(int i=0;i<ITERATIONS;i++)
+        {
+                hipMemcpyAsync(ddata,hdata,datasize*sizeof(float),hipMemcpyDeviceToHost,stream);
+        }
+        hipEventRecord(stop,stream);
+        hipStreamSynchronize(stream);
+        float milli_sec1 = 0;
+        hipEventElapsedTime(&milli_sec1,start,stop);
+        const float bandwidth_one = ((float(datasize)*sizeof(float)*float(ITERATIONS))/(milli_sec1 * 1e6));
+        printf("GPU to CPU Bandwidth is :%0.3f GB/s\n",bandwidth_one);
+
+	hipEventCreate(&start);
+        hipEventCreate(&stop);
+        hipEventRecord(start,stream);
+        for(int i=0;i<ITERATIONS;i++)
+        {
+                hipMemcpyAsync(ddata,hdata,datasize*sizeof(float),hipMemcpyDeviceToDevice,stream);
+        }
+        hipEventRecord(stop,stream);
+        hipStreamSynchronize(stream);
+        float milli_sec2 = 0;
+        hipEventElapsedTime(&milli_sec2,start,stop);
+        const float bandwidth_two = ((float(datasize)*sizeof(float)*float(ITERATIONS))/(milli_sec2 * 1e6));
+        printf("GPU to GPU Bandwidth is :%0.3f GB/s\n",bandwidth_two);
+
+	hipEventCreate(&start);
+        hipEventCreate(&stop);
+        hipEventRecord(start,stream);
+        for(int i=0;i<ITERATIONS;i++)
+        {
+                hipMemcpyAsync(ddata,hdata,datasize*sizeof(float),hipMemcpyHostToHost,stream);
+        }
+        hipEventRecord(stop,stream);
+        hipStreamSynchronize(stream);
+        float milli_sec3 = 0;
+        hipEventElapsedTime(&milli_sec3,start,stop);
+        const float bandwidth_three = ((float(datasize)*sizeof(float)*float(ITERATIONS))/(milli_sec3 * 1e6));
+        printf("CPU to CPU Bandwidth is :%0.3f GB/s\n",bandwidth_three);
+
+
+
+
+	hipFree(ddata);
+	delete[] hdata;
+	hipEventDestroy(start);
+	hipEventDestroy(stop);
+}
+
+int main()
+{
+	hipStream_t stream;
+	hipStreamCreate(&stream);
+	const size_t datasize= 1<<20;
+	checkbandwidth(stream,datasize);
+	hipStreamDestroy(stream);
+	return 0;
+}
+
+
